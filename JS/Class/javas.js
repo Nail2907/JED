72b3c188643e4e7809ecc011e0323ad87c349d96
Nail Gambarli <nailqmbrli@gmail.com>
2023-08-15 12:39:41 +0400

    yene commit

--- JS/Class/javas.js
+++ JS/Class/javas.js
@@ -55,9 +55,12 @@
 //         break;
 // }
 
+let a = 0
 
-// for(let i=1; i<=100; i++){
-//     if(i%2===1){
-//         console.log(i)
-//     }
-// }
+for(let i=1; i<=100; i++){
+    if(i%2===1){
+        console.log(i)
+        a += i
+    }
+}
+console.log(a)
